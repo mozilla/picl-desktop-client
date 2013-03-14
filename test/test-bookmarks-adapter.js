9657e7be933235ce1aa1b73f70d9dcdf2cae0ea9
Chris Karlof <ckarlof@mozilla.com>
2013-03-14 15:58:56 -0700

    refactor of 'update bookmarks' tests

--- test/test-bookmarks-adapter.js
+++ test/test-bookmarks-adapter.js
@@ -12,7 +12,7 @@ const TEST_BOOKMARK_DATA =[
 {"parentid":"S0cKErf89YfK","localType":1,"pos":3,"title":"Get Involved","bmkUri":"http://www.mozilla.com/en-US/firefox/community/","type":"bookmark","id":"GmuNI1NZ8hRe","timestamp":1363214160099,"version":52},
 {"parentid":"S0cKErf89YfK","localType":1,"pos":4,"title":"About Us","bmkUri":"http://www.mozilla.com/en-US/about/","type":"bookmark","id":"R6bkPmTfUyEi","timestamp":1363214160099,"version":52},
 {"parentid":"toolbar","localType":1,"pos":0,"title":"Most Visited","bmkUri":"place:sort=8&maxResults=10","queryId":"MostVisited","folderName":"Most Visited","type":"query","id":"1pzDy639sJ8D","timestamp":1363214160099,"version":52},
-{"parentid":"menu","localType":1,"pos":0,"title":"Recently Bookmarked","bmkUri":"place:folder=BOOKMARKS_MENU&folder=UNFILED_BOOKMARKS&folder=TOOLBAR&queryType=1&sort=12&maxResults=10&excludeQueries=1","queryId":"RecentlyBookmarked","folderName":"Recently Bookmarked","type":"query","id":"x-MMaZ2iIm2E","timestamp":1363214160099,"version":52},
+{"parentid":"menu","localType":1,"pos":0,"title":"Recently Bookmarked","bmkUri":"place:folder=BOOKMARKS_MENU&folder=UNFILED_BOOKMARKS&folder=TOOLBAR&sort=12&excludeQueries=1&maxResults=10&queryType=1","queryId":"RecentlyBookmarked","folderName":"Recently Bookmarked","type":"query","id":"x-MMaZ2iIm2E","timestamp":1363214160099,"version":52},
 {"parentid":"menu","localType":1,"pos":1,"title":"Recent Tags","bmkUri":"place:type=6&sort=14&maxResults=10","queryId":"RecentTags","folderName":"Recent Tags","type":"query","id":"Rpt_3c8ALtPX","timestamp":1363214160099,"version":52},
 {"parentid":"menu","localType":3,"pos":2,"type":"separator","id":"a8MBO_X-SG-l","timestamp":1363214160099,"version":52},
 {"parentid":"toolbar","localType":1,"pos":1,"title":"Yahoo!","bmkUri":"http://www.yahoo.com/","keyword":"yahoo","type":"bookmark","id":"A3ghuMeT2pv1","timestamp":1363214160099,"version":52},
@@ -40,61 +40,77 @@ const TEST_BOOKMARK_DATA =[
 {"parentid":"places","localType":2,"pos":3,"title":"Unsorted Bookmarks","children":["4ZYFoWVc5qvD"],"type":"folder","id":"unfiled","timestamp":1363214160099,"version":52}
 ];
 
-function verifyBookmarksToolbar(assert) {
-  var result = PlacesUtils.getFolderContents(PlacesUtils.bookmarks.toolbarFolder);
-  var rootNode = result.root;
-  var node;
-  var query;
-  node = rootNode.getChild(0);
-  assert.ok(PlacesUtils.nodeIsQuery(node), "First toolbar item should be a query");
+function verifyBookmark(assert, node, info) {
+  assert.ok(PlacesUtils.nodeIsBookmark(node), info.initialMsg || "Item should be a bookmark");
+  assert.equal(node.title, info.title, "Bookmark should have the correct title");
+  assert.equal(node.uri, info.uri, "Bookmark should have the correct uri");
+  if (info.tags !== undefined) {
+    assert.equal(node.tags, info.tags, "Bookmark should have the correct tags");
+  }
+  if (info.keyword) {
+    assert.equal(PlacesUtils.bookmarks.getKeywordForBookmark(node.itemId), info.keyword, "Bookmark should have the correct keyword");
+  }
+  if (info.loadInSidebar) {
+    assert.ok(PlacesUtils.annotations.getItemAnnotation(node.itemId, "bookmarkProperties/loadInSidebar"), "Bookmark should have loadInSidebar set");
+  }
+  if (info.description) {
+    assert.equal(PlacesUtils.annotations.getItemAnnotation(node.itemId, "bookmarkProperties/description"), info.description, "Bookmark should have the correct description");
+  }
+}
+
+function verifyQuery(assert, node, info) {
+  assert.ok(PlacesUtils.nodeIsQuery(node), info.initialMsg || "Item should be a query");
   query = node.QueryInterface(Ci.nsINavHistoryQueryResultNode);
-  assert.equal(query.title, "Most Visited", "First toolbar item should have the correct title");
-  assert.equal(query.uri, "place:sort=8&maxResults=10", "First toolbar item should have the correct uri");
-  node = rootNode.getChild(1);
-  assert.ok(PlacesUtils.nodeIsBookmark(node), "Second toolbar item should be a bookmark");
-  assert.equal(node.title, "Yahoo!", "Second toolbar item should have the correct title");
-  assert.equal(node.uri, "http://www.yahoo.com/", "Second toolbar item should have the correct uri");
-  assert.equal(node.tags, "tagBar, tagFoo1", "Second toolbar item should have the correct tags");
-  assert.equal(PlacesUtils.bookmarks.getKeywordForBookmark(node.itemId), "yahoo", "Second toolbar item should have the correct keyword");
-  node = rootNode.getChild(2);
-  assert.ok(PlacesUtils.nodeIsBookmark(node), "Third toolbar item should be a bookmark");
-  assert.equal(node.title, "Google", "Third toolbar item should have the correct title");
-  assert.equal(node.uri, "https://www.google.com/", "Third toolbar item should have the correct uri");
-  assert.equal(node.tags, "tagFoo2", "Third toolbar item should have the correct tags");
-  assert.ok(PlacesUtils.annotations.getItemAnnotation(node.itemId, "bookmarkProperties/loadInSidebar"), "Third toolbar item should have loadInSidebar set");
-  node = rootNode.getChild(3);
-  assert.ok(PlacesUtils.nodeIsBookmark(node), "Fourth toolbar item should be a bookmark");
-  assert.equal(node.title, "CNN.com", "Fourth toolbar item should have the correct title");
-  assert.equal(node.uri, "http://www.cnn.com/", "Fourth toolbar item should have the correct uri");
-  assert.equal(node.tags, null, "Fourth toolbar item should have the correct tags");
-  node = rootNode.getChild(4);
-  assert.ok(PlacesUtils.nodeIsBookmark(node), "Fifth toolbar item should be a bookmark");
-  assert.equal(node.title, "Facebook", "Fifth toolbar item should have the correct title");
-  assert.equal(node.uri, "https://www.facebook.com/", "Fifth toolbar item should have the correct uri");
-  assert.equal(node.tags, null, "Fifth toolbar item should have the correct tags");
-  node = rootNode.getChild(5);
+  assert.equal(query.title, info.title, "Query should have the correct title");
+  assert.ok(node.uri.indexOf("place:") == 0, "Query should have a query uri");
+}
+
+function verifyLivemark(assert, node, info) {
   assert.ok(PlacesUtils.nodeIsFolder(node) &&
             PlacesUtils.annotations
                        .itemHasAnnotation(node.itemId, PlacesUtils.LMANNO_FEEDURI) &&
             PlacesUtils.annotations
-                       .itemHasAnnotation(node.itemId, PlacesUtils.LMANNO_SITEURI), "Sixth toolbar item should be a livemark");
-  assert.equal(node.title, "Lifehacker", "Sixth toolbar item should have the correct title");
+                       .itemHasAnnotation(node.itemId, PlacesUtils.LMANNO_SITEURI), info.initialMsg || "Item should be a livemark");
+  assert.equal(node.title, info.title, "Livemark should have the correct title");
   let feedUri = PlacesUtils.annotations.getItemAnnotation(node.itemId, PlacesUtils.LMANNO_FEEDURI);
   let siteUri = PlacesUtils.annotations.getItemAnnotation(node.itemId, PlacesUtils.LMANNO_SITEURI);
-  assert.equal(feedUri, "http://feeds.gawker.com/lifehacker/full", "Sixth toolbar item should have the correct feed uri");
-  assert.equal(siteUri, "http://lifehacker.com/", "Sixth toolbar item should have the correct site uri");
+  assert.equal(feedUri, info.feedUri, "Livemark should have the correct feed uri");
+  assert.equal(siteUri, info.siteUri, "Livemark should have the correct site uri");
+}
+
+function verifyFolder(assert, node, info) {
+  assert.ok(PlacesUtils.nodeIsFolder(node), info.initialMsg || "Item should be a folder");
+  assert.equal(node.title, info.title, "Folder should have the correct title");
+}
+
+function verifySeparator(assert, node, info) {
+  assert.ok(PlacesUtils.nodeIsSeparator(node), info.initialMsg || "Item should be a separator");
+}
+
+function verifyBookmarksToolbar(assert) {
+  var result = PlacesUtils.getFolderContents(PlacesUtils.bookmarks.toolbarFolder);
+  var rootNode = result.root;
+  var node;
+
+  node = rootNode.getChild(0);
+  verifyQuery(assert, node, { initialMsg: "First toolbar item should be a query", title: "Most Visited" });
+  node = rootNode.getChild(1);
+  verifyBookmark(assert, node, { initialMsg: "Second toolbar item should be a bookmark", title: "Yahoo!", uri: "http://www.yahoo.com/", tags: "tagBar, tagFoo1", keyword: "yahoo" });
+  node = rootNode.getChild(2);
+  verifyBookmark(assert, node, { initialMsg: "Third toolbar item should be a bookmark", title: "Google", uri: "https://www.google.com/", tags: "tagFoo2", loadInSidebar: true });
+  node = rootNode.getChild(3);
+  verifyBookmark(assert, node, { initialMsg: "Fourth toolbar item should be a bookmark", title: "CNN.com", uri: "http://www.cnn.com/", tags: null });
+  node = rootNode.getChild(4);
+  verifyBookmark(assert, node, { initialMsg: "Fifth toolbar item should be a bookmark", title: "Facebook", uri: "https://www.facebook.com/", tags: null });
+  node = rootNode.getChild(5);
+  verifyLivemark(assert, node, { initialMsg: "Sixth toolbar item should be a livemark", title: "Lifehacker", feedUri: "http://feeds.gawker.com/lifehacker/full", siteUri: "http://lifehacker.com/"});
   node = rootNode.getChild(6);
-  assert.ok(PlacesUtils.nodeIsFolder(node), "Seventh toolbar item should be a folder");
-  assert.equal(node.title, "testFolder", "Seventh toolbar item should have the correct title");
+  verifyFolder(assert, node, { initialMsg: "Seventh toolbar item should be a folder", title: "testFolder" });
   folderResult1 = PlacesUtils.getFolderContents(node.itemId);
   node = folderResult1.root.getChild(0);
-  assert.ok(PlacesUtils.nodeIsBookmark(node), "First child of seventh toolbar item should be a bookmark");
-  assert.equal(node.title, "Hulu.comff", "First child of seventh toolbar item should have the correct title");
-  assert.equal(node.uri, "http://www.hulu.com/", "First child of seventh toolbar item should have the correct uri");
-  assert.equal(node.tags, "tagBar2", "First child of seventh toolbar item should have the correct tags");
+  verifyBookmark(assert, node, { initialMsg: "First child of seventh toolbar item should be a bookmark", title: "Hulu.comff", uri: "http://www.hulu.com/", tags: "tagBar2" });
   node = folderResult1.root.getChild(1);
-  assert.ok(PlacesUtils.nodeIsFolder(node), "Second child of seventh toolbar item should be a folder");
-  assert.equal(node.title, "innerFolder", "Second child of seventh toolbar item should have the correct title");
+  verifyFolder(assert, node, { initialMsg: "Second child of seventh toolbar item should be a folder", title: "innerFolder" });
   let folderResult2 = PlacesUtils.getFolderContents(node.itemId);
   assert.ok(folderResult2.root.childCount === 0, "Second child of seventh toolbar item should have no children");
   folderResult2.root.containerOpen = false;
@@ -103,12 +119,42 @@ function verifyBookmarksToolbar(assert) {
 }
 
 function verifyBookmarksMenu(assert) {
-
+  var result = PlacesUtils.getFolderContents(PlacesUtils.bookmarks.bookmarksMenuFolder);
+  var rootNode = result.root;
+  var node;
+  var folderResult1;
+  node = rootNode.getChild(0);
+  verifyQuery(assert, node, { initialMsg: "First menu item should be a query", title: "Recently Bookmarked" });
+  node = rootNode.getChild(1);
+  verifyQuery(assert, node, { initialMsg: "Second menu item should be a query", title: "Recent Tags" });
+  node = rootNode.getChild(2);
+  verifySeparator(assert, node, { initialMsg: "Third menu item should be a separator" });
+  node = rootNode.getChild(3);
+  verifyFolder(assert, node, { initialMsg: "Fourth menu item should be a folder", title: "Mozilla Firefox" });
+  folderResult1 = PlacesUtils.getFolderContents(node.itemId);
+  node = folderResult1.root.getChild(0);
+  verifyBookmark(assert, node, { initialMsg: "First child of fourth menu item should be a bookmark", title: "Help and Tutorials", uri: "http://www.mozilla.com/en-US/firefox/help/", tags: null });
+  node = folderResult1.root.getChild(1);
+  verifyBookmark(assert, node, { initialMsg: "Second child of fourth menu item should be a bookmark", title: "Customize Firefox", uri: "http://www.mozilla.com/en-US/firefox/customize/", tags: null });
+  node = folderResult1.root.getChild(2);
+  verifySeparator(assert, node, { initialMsg: "Third child of fourth menu item should be a separator" });
+  node = folderResult1.root.getChild(3);
+  verifyBookmark(assert, node, { initialMsg: "Fourth child of fourth menu item should be a bookmark", title: "Get Involved", uri: "http://www.mozilla.com/en-US/firefox/community/", tags: null });
+  node = folderResult1.root.getChild(4);
+  verifyBookmark(assert, node, { initialMsg: "Second child of fourth menu item should be a bookmark", title: "About Us", uri: "http://www.mozilla.com/en-US/about/", tags: null });
+  folderResult1.root.containerOpen = false;
+  rootNode.containerOpen = false;
 }
 
 function verifyBookmarksUnfiled(assert) {
-
+  var result = PlacesUtils.getFolderContents(PlacesUtils.bookmarks.unfiledBookmarksFolder);
+  var rootNode = result.root;
+  var node;
+  node = rootNode.getChild(0);
+  verifyBookmark(assert, node, { initialMsg: "First unfiled folder item should be a bookmark", title: "Bank of America | Home | Personal", uri: "https://www.bankofamerica.com/", tags: null, "description":"Welcome to Bank of America, the nation's leading financial institution and home for all of your personal financial needs." });
+  rootNode.containerOpen = false;
 }
+
 
 exports['test update local bookmarks'] = function (assert, done) {
   let items = TEST_BOOKMARK_DATA.map(function (item) { return BookmarksItem(item); });
