8bc48cf1946ed6c36ee5ff89df7ebd45653113f5
Chris Karlof <ckarlof@mozilla.com>
2013-04-10 17:50:22 -0700

    basic history syncing; likely performance problems though

--- test/test-history-adapter.js
+++ test/test-history-adapter.js
@@ -1,5 +1,6 @@
 const { Cc, Ci, Cu, CC } = require("chrome");
 const HistoryAdapter = require('history-adapter');
+const TEST_HISTORY_DATA = require('data').TEST_HISTORY_DATA;
 const _ = require('underscore');
 const L = require('logger');
 
@@ -7,7 +8,7 @@ const L = require('logger');
 exports['test read local history'] = function (assert, done) {
   HistoryAdapter.read().
   then(function (results) {
-    L.log("History results", results);
+    L.log("History results", JSON.stringify(results));
     assert.ok(true);
     done();
   }).
@@ -15,5 +16,19 @@ exports['test read local history'] = function (assert, done) {
     L.log("Error", err);
   });
 }
+
+exports['test update local history'] = function (assert, done) {
+  HistoryAdapter.update(TEST_HISTORY_DATA).
+  then(function (results) {
+    L.log("History update results", results);
+    assert.ok(true);
+    done();
+  }).
+  then(null, function (err) {
+    L.log("Error in calling HistoryAdapter.update", err.message || err);
+  });
+}
+
+
 
 require("sdk/test").run(exports);
